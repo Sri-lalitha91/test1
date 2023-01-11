3d9b846da3323e210b280d08d80c853b650a9dce
sri lalitha <lalitha.gadepalli@gmail.com>
2023-01-11 10:14:23 +0530

    Initial commit

---
+++ demo1.java
@@ -0,0 +1,20 @@
+package test1;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+	
+	addtab(25,76);	
+	addtab(7,8);
+	}
+	public static void addtab(int m,int n) {
+	
+
+	System.out.println(m+n);
+	System.out.println(m*n);
+	}
+	
+	
+	}
+
+
